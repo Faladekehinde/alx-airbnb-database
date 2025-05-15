31ed4b839befd2da3f9636a20146b3d0a5f43b35
Faladekehinde <dafaladexy@gmail.com>
2025-05-15 19:06:39 +0100

    Adding database_index.sql, index_performance.md

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,18 @@
+-- User table indexes
+
+CREATE UNIQUE INDEX idx_user_email ON User(email);
+CREATE INDEX idx_user_id ON User(id);
+
+-- Booking table indexes
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id)
+CREATE INDEX idx_booking_dates ON Booking(start_date, end_date);
+
+-- Property table indexes
+CREATE INDEX idx_property_id ON Property(id);
+CREATE INDEX idx_property_location ON Property(location);
+CREATE INDEX Idx_property_host_id ON Property(host_id);
+
+-- Measure query performance after indexing
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 101;
+EXPLAIN ANALYZE SELECT * FROM Property WHERE location = 'Abule Egba' AND price < 5000;
