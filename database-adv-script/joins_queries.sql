b94d74ff2d358ec948ab165bb014d09d71183ca1
Faladekehinde <dafaladexy@gmail.com>
2025-05-15 15:37:49 +0100

    Adding FULL OUTER JOIN

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -34,6 +34,29 @@ ORDER BY
 -- Retreive all users and all bookings even if the user has no booking
 -- or a booking is not linked to a user
 
+SELECT
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email,
+    Booking.booking_id,
+    Booking.property_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status
+FROM
+    User
+FULL OUTER JOIN
+    Booking ON User.user_id = Booking.user_id
+ORDER BY
+    User.first_name ASC;
+
+
+OR
+
+
+
 SELECT 
     User.user_id,
     User.first_name,
